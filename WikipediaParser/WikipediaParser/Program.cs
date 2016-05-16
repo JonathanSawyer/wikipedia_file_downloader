74b3ad1b750c7d94026e955a9b8176ac748ed210
JonathanSawyer <JonathanSawyer@users.noreply.github.com>
2016-05-16 15:45:41 +0900

    Initial Commit

---
+++ WikipediaParser/WikipediaParser/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace WikipediaParser
+{
+    class Program
+    {
+        static ManualResetEvent resetEvent = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            string[] topics = ConfigurationManager.AppSettings["topics"].ToLower().Split(',');            
+
+            int numThreads = topics.Count();           
+
+            for (int index = 0; index < numThreads; index++)
+            {
+                var topic = topics[index];
+                if(!String.IsNullOrWhiteSpace(topic))
+                    new Thread(() => DownloadContent(topic, ref numThreads)).Start();
+            }
+            resetEvent.WaitOne();
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+        }
+
+        private static void DownloadContent(string topic, ref int numThreads) 
+        {
+            Console.WriteLine("Downloading " + topic + "...");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format(ConfigurationManager.AppSettings["wikipedia_url"], topic));
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (StreamReader responseReader = new StreamReader(response.GetResponseStream()))
+            {
+                string jsonResult = responseReader.ReadToEnd();
+                var json = JObject.Parse(jsonResult);
+                responseReader.Close();
+
+                StringBuilder builder = new StringBuilder();
+                foreach (var children in json["query"]["pages"].Children())
+                {
+                    builder.Append(children.Children().ToArray()[0]["extract"]);
+                }
+
+                if (!String.IsNullOrWhiteSpace(builder.ToString()))
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(ConfigurationManager.AppSettings["output_target"] + topic + ".txt"))
+                {
+                    file.Write(builder.ToString());
+                }
+            }
+
+            if (Interlocked.Decrement(ref numThreads) == 0)
+                resetEvent.Set();
+
+            Console.WriteLine("Finished " + topic + ".");
+        }
+    }
+}
---
+++ WikipediaParser/WikipediaParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WikipediaParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WikipediaParser")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8145c783-6b17-445f-b4b3-b90fd0c3a86c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
